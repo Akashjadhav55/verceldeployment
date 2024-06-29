37ea13f50c03139804e3e01104dce97bf4670aec
Akash Jadhav <87018998+Akashjadhav55@users.noreply.github.com>
2024-06-29 12:35:00 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+let express = require("express")
+
+let app = express()
+
+app.get("/", (req ,res) => {
+  res.send("hello")
+})
+
+app.listen(3000, () => {
+  console.log("server is live on port: 3000")
+})
